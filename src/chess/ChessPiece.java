313541e487f03a829d7a4241156105122a343c3b
Jackson Ramos <jackzorg.256@gmail.com>
2023-09-16 20:29:45 -0300

    final product

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -6,20 +6,36 @@ import BoardGame.Position;
 
 public abstract class ChessPiece extends Piece {
 
+	private Color color;
+	private int moveCount;
 
-private Color color;
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+	}
 
-public ChessPiece(Board board, Color color) {
-	super(board);
-	this.color = color;
-}
+	public Color getColor() {
+		return color;
+	}
+	
+	public int getMoveCount() {
+		return moveCount;
+	}
+	
+	protected void increaseMoveCount() {
+		moveCount++;
+	}
 
-public Color getColor() {
-	return color;
-}
+	protected void decreaseMoveCount() {
+		moveCount--;
+	}
 
-protected boolean isThereOpponentPiece(Position position) {
-	ChessPiece p = (ChessPiece)getBoard().piece(position);
-    return p != null && p.getColor() !=color;
-} 
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position);
+	}
+	
+	protected boolean isThereOpponentPiece(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p != null && p.getColor() != color;
+	}
 }
