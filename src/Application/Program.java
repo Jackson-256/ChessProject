084307a767f7ad24390cabd73bbaa6b445242c02
Jackson Ramos <jackzorg.256@gmail.com>
2023-09-15 22:03:24 -0300

    Priting possible moves

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -22,6 +22,10 @@ public class Program {
 				System.out.println();
 				System.out.print("source: ");
 				ChessPosition source = UI.readChessPosition(sc);
+				boolean [][] possibleMoves = chessmatch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(chessmatch.getPieces(), possibleMoves);
+				
 				System.out.println();
 				System.out.print("target: ");
 				ChessPosition target = UI.readChessPosition(sc);
